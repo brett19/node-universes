bb8e4b3e13fbe3b95d921916f97366a5920c35d6
Brett Lawson <brett19@gmail.com>
2013-12-27 08:08:11 -0400

    Significant bootstrapping changes, see mousegame example changes.

--- examples/mousegame/server.js
+++ examples/mousegame/server.js
@@ -1,91 +1,89 @@
-var universes = require('../../lib/universes');
-
-var log = universes.logger;
-var UniApp = universes.App;
-var UniPrimus = universes.Primus;
-var UniRooms = universes.Rooms;
-
-var options = {
+require('../../lib/universes')({
+  workers: 2,
   couchbase: {
     host: 'localhost:8091',
     bucket: 'default',
     password: ''
   }
-};
+}, function(app) {
+  var log = app.logger;
+  var UniApp = app.App;
+  var UniPrimus = app.Primus;
+  var UniRooms = app.Rooms;
 
-var app = new UniApp(options);
-var ws = new UniPrimus(app);
-var rooms = new UniRooms(app);
+  var ws = new UniPrimus(app);
+  var rooms = new UniRooms(app);
 
-app.static('/logos', './logos');
-app.static('/client', __dirname + '/client');
+  app.static('/logos', './logos');
+  app.static('/client', __dirname + '/client');
 
-app.non('join', function(client, args) {
-  client.name = args.name;
-  client.color = args.color;
-  client.x = args.x;
-  client.y = args.y;
+  app.non('join', function(client, args) {
+    client.name = args.name;
+    client.color = args.color;
+    client.x = args.x;
+    client.y = args.y;
 
-  var room = rooms.findRoom('main');
-  if (!room) {
-    rooms.createRoom('main', client);
-  } else {
-    room.addClient(client);
-  }
-
-  client.nemit('joined');
-});
-
-rooms.on('clientJoined', function(room, client) {
-  log.debug('rooms:clientJoined', room.uuid, client.uuid);
-
-  // Send my addplayer to everyone except me
-  room.nemit(client, 'addplayer', {
-    uuid: client.uuid,
-    name: client.name,
-    color: client.color,
-    x: client.x,
-    y: client.y
-  });
-
-  for (var i = 0; i < room.clients.length; ++i) {
-    var oclient = room.clients[i];
-
-    // Don't update me about myself
-    if (oclient === client) {
-      continue;
+    var room = rooms.findRoom('main');
+    if (!room) {
+      rooms.createRoom('main', client);
+    } else {
+      room.addClient(client);
     }
 
-    // Tell this client about another
-    client.nemit('addplayer', {
-      uuid: oclient.uuid,
-      name: oclient.name,
-      color: oclient.color,
-      x: oclient.x,
-      y: oclient.y
+    client.nemit('joined');
+  });
+
+  rooms.on('clientJoined', function(room, client) {
+    log.debug('rooms:clientJoined', room.uuid, client.uuid);
+
+    // Send my addplayer to everyone except me
+    room.nemit(client, 'addplayer', {
+      uuid: client.uuid,
+      name: client.name,
+      color: client.color,
+      x: client.x,
+      y: client.y
     });
-  }
-});
-rooms.on('clientLeft', function(room, client) {
-  log.debug('rooms:clientLeft', room.uuid, client.uuid);
 
-  room.nemit('delplayer', {
-    uuid: client.uuid
+    for (var i = 0; i < room.clients.length; ++i) {
+      var oclient = room.clients[i];
+
+      // Don't update me about myself
+      if (oclient === client) {
+        continue;
+      }
+
+      // Tell this client about another
+      client.nemit('addplayer', {
+        uuid: oclient.uuid,
+        name: oclient.name,
+        color: oclient.color,
+        x: oclient.x,
+        y: oclient.y
+      });
+    }
   });
-});
+  rooms.on('clientLeft', function(room, client) {
+    log.debug('rooms:clientLeft', room.uuid, client.uuid);
 
-rooms.non('moveTo', function(room, client, cmd, args) {
-  client.x = args.x;
-  client.y = args.y;
-
-  room.nemit(client, 'moveTo', {
-    uuid: client.uuid,
-    x: client.x,
-    y: client.y
+    room.nemit('delplayer', {
+      uuid: client.uuid
+    });
   });
-});
 
-app.on('ready', function() {
-  log.info('Server Ready');
+  rooms.non('moveTo', function(room, client, cmd, args) {
+    client.x = args.x;
+    client.y = args.y;
+
+    room.nemit(client, 'moveTo', {
+      uuid: client.uuid,
+      x: client.x,
+      y: client.y
+    });
+  });
+
+  app.on('ready', function() {
+    log.info('Server Ready (', app.httpPort, ',', app.selfPort, ')');
+  });
+  app.start();
 });
-app.start();
